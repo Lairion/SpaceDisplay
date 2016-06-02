1ea2c3ba59af1a5dd6b5ee46fa591f210521263a
Anton <Anton>
2016-06-02 21:53:48 +0300

    Add new model crew

--- shipdisplay/admin.py
+++ shipdisplay/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import SystemDB,PlanetDB
+from .models import SystemDB,PlanetDB,CrewDB
 # Register your models here.
 admin.site.register(SystemDB)
 admin.site.register(PlanetDB)
+admin.site.register(CrewDB)
--- shipdisplay/models.py
+++ shipdisplay/models.py
@@ -4,20 +4,29 @@ from django.db import models
 
 # Create your models here.
 class SystemDB(models.Model):
-	name_system = models.CharField(max_length=100)
-	how_many_planets = models.IntegerField(max_length=3)
-	color_star = models.CharField(max_length=100)
-	pos_x = models.IntegerField(max_length=10)
-	pos_y = models.IntegerField(max_length=10)
+    name_system = models.CharField(max_length=100)
+    how_many_planets = models.IntegerField(max_length=3)
+    color_star = models.CharField(max_length=100)
+    pos_x = models.IntegerField(max_length=10)
+    pos_y = models.IntegerField(max_length=10)
 
 class PlanetDB(models.Model):
-	SIZE_PLANET = (("S","Small"),("M","Medium"),("L","Large"))
-	system = models.ForeignKey(SystemDB)
-	planet_name = models.CharField(max_length=100)
-	size_planet = models.CharField(max_length=20, choices=SIZE_PLANET)
-	color_planet = models.CharField(max_length=20)
-	population = models.IntegerField(max_length=100)
-	planet_position = models.IntegerField(max_length=3)
+    SIZE_PLANET = (("S","Small"),("M","Medium"),("L","Large"))
+    system = models.ForeignKey(SystemDB)
+    planet_name = models.CharField(max_length=100)
+    size_planet = models.CharField(max_length=20, choices=SIZE_PLANET)
+    color_planet = models.CharField(max_length=20)
+    population = models.IntegerField(max_length=100)
+    planet_position = models.IntegerField(max_length=3)
+class CrewDB(models.Model):
+    JOB_NAME=(("C","Captan"),("M","Mechanik"),("P","Pilot"))
+    job_in_crew = models.CharField(max_length=20, choices=JOB_NAME)
+    position= models.CharField(max_length=20,null=True)
+    number=models.IntegerField(max_length=3)
+    name=models.CharField(max_length=30)
+    
+    
+    
 
 
 		
