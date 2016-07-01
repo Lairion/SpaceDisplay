fa793b79649f77d47278fac2c9af37ecac3929c7
Anton <Anton>
2016-07-01 18:12:07 +0300

    Added new page

--- shipdisplay/views.py
+++ shipdisplay/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from shipdisplay.models import CrewDB, PeopleDB
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 # Create your views here.
 def base(request):
 	title = 'Space Ship'
@@ -13,6 +14,20 @@ def desktop(request):
     context = {
         "title": title, 
     }
+    if request.method = "GET":
+        if request.GET['view'] == "Galaxy map":
+            return HttpResponseRedirect()
+            
+        else if request.GET['view'] == "Galaxy map":
+        else if request.GET['view'] == "Spaceship":
+        else if request.GET['view'] == "Connect":
+        else if request.GET['view'] == "Crew":
+        else if request.GET['view'] == "Storage":
+        else if request.GET['view'] == "Setting":
+        else if request.GET['view'] == "Drive":
+        else if request.GET['view'] == "Research lab":
+        else if request.GET['view'] == "Sickbay":
+        else if request.GET['view'] == "About OS":
             
             
     return render(request,"desktop.html", context)
